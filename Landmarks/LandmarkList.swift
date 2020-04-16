d438e1ce897dae876bebb8be9f9cab191e2d16af
brokoli <bocalist@gmail.com>
2020-04-16 23:37:48 +0900

    add all other files

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Hyunseung Choi on 2020/04/16.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData, id: \.id) { landmark in NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
